c54710fc043bcacbc50a3a7406be8b6a2763c00f
Fernando Fraga <fernandofraga109@gmail.com>
2017-01-22 23:03:54 -0200

    Tests with menus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import { MenuTestPage } from '../pages/menu-test/menu-test';
 
 
 @Component({
@@ -10,6 +11,7 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
   rootPage = HomePage;
+  pages: Array<{component: any, title: string, icon: string}>;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
@@ -18,5 +20,19 @@ export class MyApp {
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+
+    this.pages = [
+      {component: HomePage, title: 'Home', icon: 'home'},
+      {component: MenuTestPage, title: 'Menu Test', icon: 'menu'}
+    ];
+  }
+  //Exemple action to class
+  clicked() : void {
+    console.log('I was clicked!');
+  }
+
+  openPage(page: any) : void {
+    console.log('Opening '+page.title);
+    this.rootPage = page.component;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MenuTestPage } from '../pages/menu-test/menu-test';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    MenuTestPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    MenuTestPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
