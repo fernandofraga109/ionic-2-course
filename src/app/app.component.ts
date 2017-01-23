d3261eeb4ffbbde01c47e941ccbeddcb7468e970
Fernando Fraga <fernandofraga109@gmail.com>
2017-01-23 16:03:06 -0200

    Just src

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
 import { MenuTestPage } from '../pages/menu-test/menu-test';
+import { PerfilPage } from '../pages/perfil/perfil';
+import { EnderecosPage } from '../pages/enderecos/enderecos';
+import { UltimosPedidosPage } from '../pages/ultimos-pedidos/ultimos-pedidos';
+import { FaleConoscoPage } from '../pages/fale-conosco/fale-conosco';
+import { AjudaPage } from '../pages/ajuda/ajuda';
+import { ConfiguracoesPage } from '../pages/configuracoes/configuracoes';
+import { SairPage } from '../pages/sair/sair';
 
 
 @Component({
@@ -13,7 +20,7 @@ export class MyApp {
   rootPage = HomePage;
   pages: Array<{component: any, title: string, icon: string}>;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, private menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -23,7 +30,13 @@ export class MyApp {
 
     this.pages = [
       {component: HomePage, title: 'Home', icon: 'home'},
-      {component: MenuTestPage, title: 'Menu Test', icon: 'menu'}
+      {component: PerfilPage, title: 'Perfil', icon: 'menu'},
+      {component: EnderecosPage, title: 'Meus Endereços', icon: 'menu'},
+      {component: UltimosPedidosPage, title: 'Últimos Pedidos', icon: 'menu'},
+      {component: FaleConoscoPage, title: 'Fale Conosco', icon: 'menu'},
+      {component: AjudaPage, title: 'Ajuda', icon: 'menu'},
+      {component: ConfiguracoesPage, title: 'Configurações', icon: 'menu'},
+      {component: SairPage, title: 'Sair', icon: 'menu'}
     ];
   }
   //Exemple action to class
@@ -34,5 +47,6 @@ export class MyApp {
   openPage(page: any) : void {
     console.log('Opening '+page.title);
     this.rootPage = page.component;
+    this.menuCtrl.close();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,27 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { MenuTestPage } from '../pages/menu-test/menu-test';
+import { PerfilPage } from '../pages/perfil/perfil';
+import { EnderecosPage } from '../pages/enderecos/enderecos';
+import { UltimosPedidosPage } from '../pages/ultimos-pedidos/ultimos-pedidos';
+import { FaleConoscoPage } from '../pages/fale-conosco/fale-conosco';
+import { AjudaPage } from '../pages/ajuda/ajuda';
+import { ConfiguracoesPage } from '../pages/configuracoes/configuracoes';
+import { SairPage } from '../pages/sair/sair';
+
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    MenuTestPage
+    MenuTestPage,
+    PerfilPage,
+    EnderecosPage,
+    UltimosPedidosPage,
+    FaleConoscoPage,
+    AjudaPage,
+    ConfiguracoesPage,
+    SairPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -17,7 +32,14 @@ import { MenuTestPage } from '../pages/menu-test/menu-test';
   entryComponents: [
     MyApp,
     HomePage,
-    MenuTestPage
+    MenuTestPage,
+    PerfilPage,
+    EnderecosPage,
+    UltimosPedidosPage,
+    FaleConoscoPage,
+    AjudaPage,
+    ConfiguracoesPage,
+    SairPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
