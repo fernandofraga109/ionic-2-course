c2802a2772173636fed14281e94f430a5daf295f
Fernando Fraga <fernandofraga109@gmail.com>
2017-02-03 16:26:11 -0200

    Fixing in list produtct, Alert of confimation

--- src/pages/cesta/cesta.ts
+++ src/pages/cesta/cesta.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { ProdutosClienteService } from '../../providers/produtos-cliente-service';
 
 
@@ -11,7 +11,8 @@ export class CestaPage {
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public produtosClienteService :ProdutosClienteService) {}
+              public produtosClienteService :ProdutosClienteService,
+              public alertCtrl: AlertController ) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CestaPaged');
@@ -21,10 +22,38 @@ export class CestaPage {
     return this.produtosClienteService.getProdutos();
   }
   
+  getQtdProdutos() {
+    return this.produtosClienteService.getQtdProdutos();
+  }
   
   decrementaQtd(produto) : void {
-    if (this.produtosClienteService.produtoJaNaCesta(produto)) {
-      this.produtosClienteService.decrementaQtdProduto(produto);
+    if (produto.quantidade == 1) {
+      let confirm = this.alertCtrl.create({
+      title: 'Confirmacao',
+      message: 'Confirma exclusao do produto '+ produto.prod_nome  + ' ?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Confirmar',
+          handler: () => {
+            this.produtosClienteService.removeProduto(produto);
+          }
+        }
+      ]
+    });
+    confirm.present();
+  
+      
+    } else {
+       
+      if (this.produtosClienteService.produtoJaNaCesta(produto)) {
+        this.produtosClienteService.decrementaQtdProduto(produto);
+      }
     }
   }
   
